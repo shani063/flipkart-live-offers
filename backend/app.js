27273719d1f0ff824d59ebf4f88b021d58f4aa0d
maulik <shanimohammad063@gmail.com>
2024-10-23 16:41:54 +0530

    xxx

--- backend/app.js
+++ backend/app.js
@@ -29,9 +29,9 @@ app.use('/api/v1', product);
 app.use('/api/v1', order);
 app.use('/api/v1', payment);
 
-app.use(express.static(path.join(__dirname, '/frontend/build')))
+app.use(express.static(path.join(__dirname, '/frontend/public')))
 app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+    res.sendFile(path.resolve(__dirname, 'frontend', 'public', 'index.html'))
 });
 
 // error middleware

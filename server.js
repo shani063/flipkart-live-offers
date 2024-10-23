4264703b590b4ac0f443f80939d27f8697ee9d82
maulik <shanimohammad063@gmail.com>
2024-10-23 18:46:04 +0530

    SSS

--- server.js
+++ server.js
@@ -21,17 +21,11 @@ cloudinary.config({
 
 // deployment
 __dirname = path.resolve();
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '/frontend/build')))
+app.use(express.static(path.join(__dirname, '/frontend/build')))
 
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
-    });
-} else {
-    app.get('/', (req, res) => {
-        res.send('Server is Running! 🚀');
-    });
-}
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+});
 
 const server = app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`)

e8396cf38912e5c40f9d1fdb5391be18bac15b92
maulik <shanimohammad063@gmail.com>
2024-10-23 16:28:39 +0530

    op

--- backend/app.js
+++ backend/app.js
@@ -4,21 +4,18 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const fileUpload = require('express-fileupload');
 const errorMiddleware = require('./middlewares/error');
-const cors=require('cors');
+const cors = require('cors');
 const app = express();
-const dotenv=require('dotenv').config({ path: './config/.env' });
+const dotenv = require('dotenv').config({ path: './config/.env' });
 
 
-// config
-if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config({ path: 'backend/config/config.env' });
-}
+require('dotenv').config({ path: 'backend/config/config.env' });
 
 app.use(express.json());
 app.use(cookieParser('secret'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(fileUpload());
-app.use(cors({credentials: true, origin: true, withCredentials: true }))
+app.use(cors({ credentials: true, origin: true, withCredentials: true }))
 // app.use(dotenv())
 
 
@@ -29,22 +26,13 @@ const payment = require('./routes/paymentRoute');
 
 app.use('/api/v1', user);
 app.use('/api/v1', product);
-app.use('/api/v1', order); 
-app.use('/api/v1', payment); 
+app.use('/api/v1', order);
+app.use('/api/v1', payment);
 
-// deployment
-__dirname = path.resolve();
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '/frontend/build')))
-
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
-    });
-} else {
-    app.get('/', (req, res) => {
-          res.send('server on');
-    });
-}
+app.use(express.static(path.join(__dirname, '/frontend/build')))
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+});
 
 // error middleware
 app.use(errorMiddleware);
---
+++ backend/frontend/src/urlconfig.js
@@ -0,0 +1,3 @@
+const baseurl = "${window.location.origin}/api/v1"
+//http://localhost:4000/api/v1
+export default baseurl;
